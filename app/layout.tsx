26b6ae648a3164f12303d5e96294cbb5d981b3e4
hewei <weibalangzi@gmail.com>
2025-01-19 22:57:03 +0800

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager, GoogleAnalytics } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "Bot of Hewei",
+  description: "My personal DeepSeek Chat Bot.",
   appleWebApp: {
     title: "NextChat",
     statusBarStyle: "default",
